297d208ea56cab387b0a7c040c0d418893803d8c
Johan Klintsäter <johanklintsater@outlook.com>
2015-10-26 16:40:22 +0100

    xml

--- LCGBanking/LCGBanking/Licensiering.aspx
+++ LCGBanking/LCGBanking/Licensiering.aspx
@@ -1,4 +1,10 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.Master" AutoEventWireup="true" CodeBehind="Licensiering.aspx.cs" Inherits="LCGBanking.Licensiering" %>
 <asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
-    <p>blablablabladblablalflajfdskaljsdflkasjdlfjkaldsflaksjdf</p>
+    <section>  
+
+        
+        <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+    </section>
+  
 </asp:Content>
+
--- LCGBanking/LCGBanking/Licensiering.aspx.cs
+++ LCGBanking/LCGBanking/Licensiering.aspx.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
+using System.Xml;
 using System.Web.UI.WebControls;
+using System.Text;
+
 
 namespace LCGBanking
 {
@@ -11,7 +14,51 @@ namespace LCGBanking
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            visaXML();
 
         }
+    
+
+    private void visaXML()
+    {
+        string xmlfil = Server.MapPath("APP_CODE/XML_Query.xml");
+        XmlTextReader reader = new XmlTextReader(xmlfil);
+        StringBuilder str = new StringBuilder();
+
+        reader.ReadStartElement("Questions");
+
+        while (reader.Read())
+        {
+            switch (reader.NodeType)
+            {
+                case XmlNodeType.Element:
+                    str.Append("Element: ");
+                    str.Append(reader.Name);
+                    str.Append("<br />");
+
+                    if (reader.AttributeCount > 1)
+                    {
+                        while (reader.MoveToNextAttribute())
+                        { 
+                            str.Append("Attributnamn: ");
+                            str.Append(reader.Name);
+                            str.Append(": ");
+                            str.Append(reader.Value);
+                            str.Append("<br />");
+                        }
+
+                    }
+                    break;
+
+                case XmlNodeType.Text:
+                    str.Append("Fråga ");
+                    str.Append(reader.Value);
+                    str.Append("<br />");
+                    break;
+            }
+        }
+        Label1.Text = str.ToString();
+    }
+    
     }
 }
