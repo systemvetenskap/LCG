17700928fd0795c0fd5460bd810c6484910cd184
sumu1400 <sumu1400@student.miun.se>
2015-11-03 17:43:55 +0100

    Har ändrat klasen Lcg_Provtillfälle till Provtillfalle. Lagt till nya metoder i Admin.

---
+++ LCGBanking/LCGBanking/Provtillfalle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LCGBanking
+{
+    public class Provtillfalle
+    {
+        public int Id { get; set; }
+        public DateTime Datum { get; set; }
+        public string Typ_av_test { get; set; }
+        public int Provresultat { get; set; }
+        public bool Godkand { get; set; }
+        public int Fk_person_id { get; set; }
+        public int Anvandar_id { get; set; }
+        public string Anvandarnamn { get; set; } 
+    }
+}
