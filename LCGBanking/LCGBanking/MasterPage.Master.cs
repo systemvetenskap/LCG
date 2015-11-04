23ddc68a86957d448923c62c7e966ce51033aac8
Johan Klintsäter <johanklintsater@outlook.com>
2015-11-05 00:30:48 +0100

    deny users allow users access åt skogen

--- LCGBanking/LCGBanking/MasterPage.Master.cs
+++ LCGBanking/LCGBanking/MasterPage.Master.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Security;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
@@ -9,9 +10,32 @@ namespace LCGBanking
 {
     public partial class MasterPage : System.Web.UI.MasterPage
     {
+        private const string conString = "cirkus";
+        public int anvandare;
+
         protected void Page_Load(object sender, EventArgs e)
         {
-            
+            anvandare = Convert.ToInt32(Session["lcg_roll"]);
+
+            if (anvandare == 1)
+                {
+                    headertext.Visible = true;
+                    HyperLinkLicens.Visible = true;
+                }
+                else if (anvandare == 2)
+                {
+                    headertext.Visible = true;
+                    HyperLinkLicens.Visible = true;
+                    HyperLinkAdmin.Visible = true;
+                }
+
+
+                /*
+                ((Label)Master.FindControl("headertext")).Visible = false;
+                ((Label)Master.FindControl("HyperLinkLicens")).Visible = false;
+                ((Label)Master.FindControl("HyperLinkAdmin")).Visible = false;
+                 */
+            }
+                
         }
     }
-}
