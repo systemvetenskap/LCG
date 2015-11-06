af6e0b42cf313022dd0f3fcc4e9813be23c87e60
Johan Klintsäter <johanklintsater@outlook.com>
2015-11-07 00:19:22 +0100

    rena skiten är det här funkar inte på datorn

--- LCGBanking/LCGBanking/APP_CODE/GlobalValues.cs
+++ LCGBanking/LCGBanking/APP_CODE/GlobalValues.cs
@@ -12,17 +12,6 @@ namespace LCGBanking
         public static int anvandarid { get; set; }
         public static int namn { get; set; }
         public static int roles { get; set; }
-        /*string path = "Test/Licenseringstest";*/
-        
-        /*
-        if (användare == "förstanställd") 
-        {
-            path = "Test/Licenseringstest"; /
-        else
-            path = "Test/Kunskapstest";
-        }
-        */
-       // string nyProvtillfalle = String.Empty;
 
         static int _frageNr = 1;
         public static int FrageNr
@@ -39,5 +28,19 @@ namespace LCGBanking
 
         public static List<Fraga> Fragor = new List<Fraga>();
         public static List<Fraga> GVIndResLista = new List<Fraga>();
+
+            /*string path = "Test/Licenseringstest";*/
+
+            /*
+            if (användare == "förstanställd") 
+            {
+                path = "Test/Licenseringstest"; /
+            else
+                path = "Test/Kunskapstest";
+            }
+            */
+            // string nyProvtillfalle = String.Empty;
+
+        }
     }
-}
+
